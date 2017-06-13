261659bb0c0a67db31a329663811b3743d347c84
Mertin Curban Gazi <mcurban@intralinks.com>
2017-06-13 16:38:34 -0400

    First commit

---
+++ src/MPC.cpp
@@ -0,0 +1,211 @@
+#include "MPC.h"
+#include <cppad/cppad.hpp>
+#include <cppad/ipopt/solve.hpp>
+#include "Eigen-3.3/Eigen/Core"
+
+using CppAD::AD;
+
+//trial and error. I started from N= 25 and dt = 0.05 until I reached the final values of N=0 and dt =.1
+/*
+  I used the model from classroom as inspiration.
+ `fg` is a vector containing the cost and constraints.
+  `vars` is a vector containing the variable values (state & actuators).
+  at line 43 is the part of the cost based on the reference state.
+ at line 50 I minimize the use of actuators.
+ */
+size_t N = 10;
+double dt = .1;
+
+const double Lf = 2.67;
+
+double ref_cte = 0;
+double ref_epsi = 0;
+double ref_v = 40;
+
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N - 1;
+
+class FG_eval {
+public:
+    // Fitted polynomial coefficients
+    Eigen::VectorXd coeffs;
+    FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
+    
+    typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
+    void operator()(ADvector& fg, const ADvector& vars) {
+        fg[0] = 0;
+        
+        for (int i = 0; i < N; i++) {
+            fg[0] += 1000*CppAD::pow(vars[cte_start + i] - ref_cte, 2);
+            fg[0] += 1000*CppAD::pow(vars[epsi_start + i] - ref_epsi, 2);
+            fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);
+        }
+        
+        for (int i = 0; i < N - 1; i++) {
+            fg[0] += 5*CppAD::pow(vars[delta_start + i], 2);
+            fg[0] += 5*CppAD::pow(vars[a_start + i], 2);
+        }
+        
+        for (int i = 0; i < N - 2; i++) {
+            fg[0] += 100*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+            fg[0] += 10*CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
+        }
+        
+        // Initial constraints
+        //
+        // We add 1 to each of the starting indices due to cost being located at
+        // index 0 of `fg`.
+        // This bumps up the position of all the other values.
+        fg[1 + x_start] = vars[x_start];
+        fg[1 + y_start] = vars[y_start];
+        fg[1 + psi_start] = vars[psi_start];
+        fg[1 + v_start] = vars[v_start];
+        fg[1 + cte_start] = vars[cte_start];
+        fg[1 + epsi_start] = vars[epsi_start];
+        
+        // The rest of the constraints
+        for (int i = 0; i < N - 1; i++) {
+            AD<double> x1 = vars[x_start + i + 1];
+            AD<double> y1 = vars[y_start + i + 1];
+            AD<double> psi1 = vars[psi_start + i + 1];
+            AD<double> v1 = vars[v_start + i + 1];
+            AD<double> cte1 = vars[cte_start + i + 1];
+            AD<double> epsi1 = vars[epsi_start + i + 1];
+            
+            AD<double> x0 = vars[x_start + i];
+            AD<double> y0 = vars[y_start + i];
+            AD<double> psi0 = vars[psi_start + i];
+            AD<double> v0 = vars[v_start + i];
+            AD<double> cte0 = vars[cte_start + i];
+            AD<double> epsi0 = vars[epsi_start + i];
+            
+            AD<double> delta0 = vars[delta_start + i];
+            AD<double> a0 = vars[a_start + i];
+            
+            AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * x0 * x0 + coeffs[3] * x0 * x0 * x0 ;
+            AD<double> psides0 = CppAD::atan(3*coeffs[3]* x0 * x0 + 2*coeffs[2]* x0 + coeffs[1]);
+            
+
+            // Recall the equations for the model:
+            fg[2 + x_start + i] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+            fg[2 + y_start + i] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+            fg[2 + psi_start + i] = psi1 - (psi0 - v0 * delta0 / Lf * dt);
+            fg[2 + v_start + i] = v1 - (v0 + a0 * dt);
+            fg[2 + cte_start + i] =
+            cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+            fg[2 + epsi_start + i] =
+            epsi1 - ((psi0 - psides0) - v0 * delta0 / Lf * dt);
+        }
+        
+    }
+};
+
+//
+// MPC class definition implementation.
+//
+MPC::MPC() {}
+MPC::~MPC() {}
+
+vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
+    bool ok = true;
+    size_t i;
+    typedef CPPAD_TESTVECTOR(double) Dvector;
+    
+    double x = state[0];
+    double y = state[1];
+    double psi = state[2];
+    double v = state[3];
+    double cte = state[4];
+    double epsi = state[5];
+    
+
+    size_t n_vars =  N * 6 + (N - 1) * 2;
+    size_t n_constraints = N * 6;
+    
+    Dvector vars(n_vars);
+    for (int i = 0; i < n_vars; i++) {
+        vars[i] = 0;
+    }
+    
+    Dvector vars_lowerbound(n_vars);
+    Dvector vars_upperbound(n_vars);
+    for (int i = 0; i < delta_start; i++) {
+        vars_lowerbound[i] = -1.0e19;
+        vars_upperbound[i] = 1.0e19;
+    }
+    
+    for (int i = delta_start; i < a_start; i++) {
+        vars_lowerbound[i] = -1.0;
+        vars_upperbound[i] = 1.0;
+    }
+    
+    for (int i = a_start; i < n_vars; i++) {
+        vars_lowerbound[i] = -1.0;
+        vars_upperbound[i] = 1.0;
+    }
+    
+    Dvector constraints_lowerbound(n_constraints);
+    Dvector constraints_upperbound(n_constraints);
+    for (int i = 0; i < n_constraints; i++) {
+        constraints_lowerbound[i] = 0;
+        constraints_upperbound[i] = 0;
+    }
+    
+    constraints_lowerbound[x_start] = x;
+    constraints_lowerbound[y_start] = y;
+    constraints_lowerbound[psi_start] = psi;
+    constraints_lowerbound[v_start] = v;
+    constraints_lowerbound[cte_start] = cte;
+    constraints_lowerbound[epsi_start] = epsi;
+    
+    constraints_upperbound[x_start] = x;
+    constraints_upperbound[y_start] = y;
+    constraints_upperbound[psi_start] = psi;
+    constraints_upperbound[v_start] = v;
+    constraints_upperbound[cte_start] = cte;
+    constraints_upperbound[epsi_start] = epsi;
+    
+    // object that computes objective and constraints
+    FG_eval fg_eval(coeffs);
+    
+    std::string options;
+    // Uncomment this if you'd like more print information
+    options += "Integer print_level  0\n";
+    options += "Sparse  true        forward\n";
+    options += "Sparse  true        reverse\n";
+    options += "Numeric max_cpu_time          0.5\n";
+    
+    // place to return solution
+    CppAD::ipopt::solve_result<Dvector> solution;
+    
+    // solve the problem
+    CppAD::ipopt::solve<Dvector, FG_eval>(
+                                          options, vars, vars_lowerbound, vars_upperbound, constraints_lowerbound,
+                                          constraints_upperbound, fg_eval, solution);
+    
+    // Check some of the solution values
+    ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+    
+    // Cost
+    auto cost = solution.obj_value;
+    
+    vector<double> result;
+    
+    result.push_back(solution.x[delta_start]);
+    result.push_back(solution.x[a_start]);
+    
+    for (int i = 0; i < N-1; i++)
+    {
+        result.push_back(solution.x[x_start + i + 1]);
+        result.push_back(solution.x[y_start + i + 1]);
+    }
+    
+    
+    return result;
+}
